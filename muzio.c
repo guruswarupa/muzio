e3eed1bb92b45e0286937e92abe88e2a84a893aa
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-11-02 22:49:28 +0530

    fix css

--- muzio.c
+++ muzio.c
@@ -245,7 +245,7 @@ int main(int argc, char *argv[]) {
 
     main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(main_window), "Muzio");
-    gtk_window_set_default_size(GTK_WINDOW(main_window), 300, 200);
+    gtk_window_set_default_size(GTK_WINDOW(main_window), 300, 300);
     g_signal_connect(main_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     create_ui();
